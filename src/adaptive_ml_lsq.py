6049446f5b82edaa8733f8fce74d359852b1ff5f
Stjepan Salatovic <s.salatovic@yahoo.de>
2022-11-21 15:36:17 +0100

    Adaptive ML speed-up: 'arcsine' samples, reuse function evaluations

--- src/adaptive_ml_lsq.py
+++ src/adaptive_ml_lsq.py
@@ -4,10 +4,9 @@ import time
 from itertools import product
 from typing import Callable
 
+from sampling import sample_arcsine
 from lsq import get_optimal_sample_size
 from lsq import LSQ
-
-from tqdm.notebook import tqdm
 
 
 def is_downward_closed(I) -> bool:
@@ -58,33 +57,37 @@ def get_power_of_two_index_set(eta):
 def slice_index_set(I, l):
     return [eta for eta in I if seperate_index(eta)[1] == l]
 
-def estimate_work(f, l, d):
-    f_ = lambda x: f(l)(x) - f(l-1)(x) if l > 0 else f(l)(x)
-    x = np.random.rand(1, d)
-    reps = 10
+def estimate_work(f, d):
+    reps = 50
+    x = np.random.rand(reps, d)
+
     s = time.perf_counter()
-    for _ in range(reps):
-        f_(x)
+    f(x)
     e = time.perf_counter()
 
-    return (e - s) / reps
+    return e - s, reps
+
+def sum_times(times):
+    sums_ = {n: [sum(tup) for tup in zip(*t)] for n, t in times.items()}
+    means_ = {n: t / n_elem for n, (t, n_elem) in sums_.items()}
+    return means_
+
 
 def construct_multi_index_set(f: Callable, n_steps: int, d: int):
     I = [(0,) * (d + 1)]
 
-    for i in range(n_steps):
-        print(f"------- STEP {i} -------")
-        print(f"Current I: {I}")
+    func_evals = {}
+    func_eval_times = {}
+    sample = np.empty((0, d))
+
+    for _ in range(n_steps):
         A = admissible_multi_indices(I)
-        print(f"A = {A}")
-        print(f"Number of admissible multi-indices: {len(A)}")
         ratios = {}
-        for eta in tqdm(A):
+        for eta in A:
 
             # GAIN ESTIMATION
             gain = 0
             neighbours = list(set(I).intersection(set(get_neighbours(eta))))
-
             for neighbour in neighbours:
                 k, l = seperate_index(neighbour)
                 under_consideration = get_power_of_two_index_set(k)
@@ -94,10 +97,28 @@ def construct_multi_index_set(f: Callable, n_steps: int, d: int):
                     k_, _ = seperate_index(eta_)
                     V.extend(get_power_of_two_index_set(k_))
 
-                f_l = f(l)
-                f_l_ = f(l - 1) if l > 0 else lambda _: 0
-                f_ = lambda x: f_l(x) - f_l_(x)
-                model = LSQ(V).solve(f_)
+                N = get_optimal_sample_size(V, sampling="arcsine")
+
+                if N > len(sample):
+                    sample = np.vstack((sample, sample_arcsine((N - len(sample), d))))
+
+                if l in func_evals:
+                    evals = func_evals[l]
+                    if N > len(evals):
+                        s = time.perf_counter()
+                        new_evals = f(l)(sample[len(evals):N])
+                        e = time.perf_counter()
+                        func_evals[l] = np.hstack((evals, new_evals))
+                        func_eval_times[l].append((e - s, N - len(evals)))
+                else:
+                    s = time.perf_counter()
+                    new_evals = f(l)(sample[:N])
+                    e = time.perf_counter()
+                    func_evals[l] = new_evals
+                    func_eval_times[l] = [(e - s, N)]
+
+                f_ = lambda _: func_evals[l][:N] - func_evals[l - 1][:N] if l > 0 else func_evals[l][:N]
+                model = LSQ(V, sampling="arcsine").solve(f_, sample=sample[:N])
 
                 gain += np.linalg.norm(
                     [v for eta, v in zip(model.I, model.coef_) if eta in under_consideration]
@@ -106,10 +127,17 @@ def construct_multi_index_set(f: Callable, n_steps: int, d: int):
 
             # WORK ESTIMATION
             k, l = seperate_index(eta)
-            n_ = get_optimal_sample_size(slice_index_set(I, l) + [k])
-            n =  get_optimal_sample_size(slice_index_set(I, l))
-            est_work = estimate_work(f, l, d)
-            work = (n_ - n) * est_work
+            if l not in func_eval_times:
+                func_eval_times[l] = [estimate_work(f(l), d)]
+
+            times = sum_times(func_eval_times)
+
+            I_l = slice_index_set(I, l)
+            n_add = get_optimal_sample_size(I_l + [k], sampling="arcsine")
+            n =  get_optimal_sample_size(I_l, sampling="arcsine")
+
+            work = times[l] - times[l - 1] if l > 0 else times[l]
+            work *= n_add - n
 
             ratios[eta] = gain / work
 
@@ -117,21 +145,29 @@ def construct_multi_index_set(f: Callable, n_steps: int, d: int):
 
     return I
 
-def adaptive_ml_algorithm(f: Callable, n_steps: int, d: int):
-    print("Start constructing multi-index set ...")
+def adaptive_ml_algorithm(f, n_steps, d):
+    print("Constructing multi-index set ...")
     I = construct_multi_index_set(f, n_steps, d)
 
+    print("Projecting ...")
     L = max(eta[-1] for eta in I)
 
     projectors = []
     for l in range(L + 1):
-        print(f"LEVEL l = {l}")
         V = []
         for eta in slice_index_set(I, l):
             k, _ = seperate_index(eta)
             V.extend(get_power_of_two_index_set(k))
-        f_ = lambda x: f(l)(x) - f(l - 1)(x) if l > 0 else f(l)(x)
-        level_l_projector = LSQ(V).solve(f_)
+
+        N = get_optimal_sample_size(V, sampling="arcsine")
+        sample = sample[:N] if l > 0 else sample_arcsine((N, d))
+
+        f_l = f(l)
+        f_l__ = f_l_[:N] if l > 0 else np.zeros(N)  # f_{-1} := 0
+        f_l_ = f_l(sample)
+        f_ = lambda _: f_l_ - f_l__
+
+        level_l_projector = LSQ(V, sampling="arcsine").solve(f_, sample=sample)
         projectors.append(level_l_projector)
 
     return projectors
