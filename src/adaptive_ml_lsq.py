87abcf707243ecec0cd27f441d878d75cdaae6d8
Stjepan Salatovic <s.salatovic@yahoo.de>
2023-01-20 14:53:14 +0100

    Adaptive ML algorithm revised

--- src/adaptive_ml_lsq.py
+++ src/adaptive_ml_lsq.py
@@ -1,13 +1,14 @@
-from __future__ import annotations
 import numpy as np
 import time
 
 from itertools import product
 from typing import Callable, Union
 
+from polynomial_spaces import PolySpace
 from sampling import sample_arcsine
 from lsq import get_optimal_sample_size
 from lsq import LSQ
+from utils import mse
 
 
 IndexSet = list[Union[int, tuple[int, ...]]]
@@ -25,38 +26,17 @@ def is_downward_closed(I: IndexSet) -> bool:
                     return False
     return True
 
-def get_neighbours(eta: tuple[int, ...], extending=False) -> IndexSet:
+def get_neighbours(eta: tuple[int, ...], extending=False):
     """
     Returns the neighbours of `eta`, where a neighbour is defined
     as a tuple that differs in exactly one entry by one.
     """
-    neighbours = []
     for i, k in enumerate(eta):
-        eta_up, eta_down = list(eta), list(eta)
-        eta_up[i] += 1
-        neighbours.append(tuple(eta_up))
-        if not extending:
-            if k - 1 >= 0:
-                eta_down[i] -= 1
-                neighbours.append(tuple(eta_down))
-    return neighbours
-
-def admissible_multi_indices(I: IndexSet) -> IndexSet:
-    """
-    Returns the admissible multi-indices of `I`,
-    the neighbours s.t. the index set is still downward closed.
-    """
-    indices = []
-    for eta in I:
-        neighbours = get_neighbours(eta, extending=True)
-        for neighbour in neighbours:
-            if neighbour in I:
-                continue
-            I_ = I.copy()
-            I_.append(neighbour)
-            if is_downward_closed(I_):
-                indices.append(neighbour)
-    return list(set(indices))
+        eta_up = eta[:i] + (k + 1,) + eta[i+1:]
+        yield eta_up
+        if not extending and k - 1 >= 0:
+            eta_down = eta[:i] + (k - 1,) + eta[i+1:]
+            yield eta_down
 
 def seperate_index(eta: tuple[int, ...]) -> tuple[tuple[int, ...], int]:
     """Seperates an index `(k, l)` into `k` and `l`."""
@@ -67,155 +47,197 @@ def get_power_of_two_index_set(eta: tuple[int, ...]) -> IndexSet:
     Returns all multi-indices `k` s.t. the condition
     `2 ^ k - 1 <= eta < 2 ^ (k+1) - 1` is fulfilled.
     """
-    low = tuple(2 ** k - 1 for k in eta)
-    high = tuple(2 ** (k + 1) - 1 for k in eta)
+    low = tuple(int(np.ceil(2 ** k)) - 1 for k in eta)
+    high = tuple(int(np.ceil(2 ** (k + 1))) - 1 for k in eta)
     I = list(product(*[range(l, h) for (l, h) in zip(low, high)]))
     return I
-
 
 def slice_index_set(I: IndexSet, l: int) -> IndexSet:
     """Returns all multi-indices `(k, s)` in `I` for which `s = l`."""
     return [eta for eta in I if seperate_index(eta)[1] == l]
-
-def estimate_work(f: Callable, d: int) -> tuple[float, int]:
-    """Measures the computation time for 50 evaluations of `f`."""
-    reps = 50
-    x = np.random.rand(reps, d)
-
-    s = time.perf_counter()
-    _ = f(x)
-    e = time.perf_counter()
-
-    return e - s, reps
 
 def sum_times(times: dict[int, list[tuple[float, int]]]) -> dict[int, float]:
     """
     Calculates the mean of computation times given by
     a list of tuples of times and number of samples.
     """
-    sums_ = {n: [sum(tup) for tup in zip(*t)] for n, t in times.items()}
-    means_ = {n: t / n_elem for n, (t, n_elem) in sums_.items()}
+    sums_ = {n: np.sum(t, axis=0) for n, t in times.items()}
+    means_ = {n: t / reps for n, (t, reps) in sums_.items()}
     return means_
 
-def construct_multi_index_set(f: Callable, n_steps: int, d: int) -> IndexSet:
-    """
-    Constructs a problem-dependent, downward closed multi-index set `I` (see Haji-Ali et al.).
-    """
-    I = [(0,) * (d + 1)]
-
-    func_evals = {}
-    func_eval_times = {}
-    sample = np.empty((0, d))
-
-    for _ in range(n_steps):
-        A = admissible_multi_indices(I)
-        ratios = {}
-        for eta in A:
-
-            # GAIN ESTIMATION
-            gain = 0
-            neighbours = list(set(I).intersection(set(get_neighbours(eta))))
-            for neighbour in neighbours:
-                k, l = seperate_index(neighbour)
-                under_consideration = get_power_of_two_index_set(k)
-
-                V = []
-                for eta_ in slice_index_set(I, l):
-                    k_, _ = seperate_index(eta_)
-                    V.extend(get_power_of_two_index_set(k_))
-
-                N = get_optimal_sample_size(V, sampling="arcsine")
-
-                if N > len(sample):
-                    sample = np.vstack((sample, sample_arcsine((N - len(sample), d))))
-
-                if l in func_evals:
-                    evals = func_evals[l]
-                    if N > len(evals):
-                        s = time.perf_counter()
-                        new_evals = f(l)(sample[len(evals):N])
-                        e = time.perf_counter()
-                        func_evals[l] = np.hstack((evals, new_evals))
-                        func_eval_times[l].append((e - s, N - len(evals)))
-                else:
-                    s = time.perf_counter()
-                    new_evals = f(l)(sample[:N])
-                    e = time.perf_counter()
-                    func_evals[l] = new_evals
-                    func_eval_times[l] = [(e - s, N)]
-
-                f_ = lambda _: func_evals[l][:N] - func_evals[l - 1][:N] if l > 0 else func_evals[l][:N]
-                model = LSQ(V, sampling="arcsine").solve(f_, sample=sample[:N])
-
-                gain += np.linalg.norm(
-                    [v for eta, v in zip(model.I, model.coef_) if eta in under_consideration]
-                )
-            gain /= len(neighbours)
-
-            # WORK ESTIMATION
-            k, l = seperate_index(eta)
-            if l not in func_eval_times:
-                func_eval_times[l] = [estimate_work(f(l), d)]
-
-            times = sum_times(func_eval_times)
-
-            I_l = slice_index_set(I, l)
-            n_add = get_optimal_sample_size(I_l + [k], sampling="arcsine")
-            n =  get_optimal_sample_size(I_l, sampling="arcsine")
-
-            work = times[l] - times[l - 1] if l > 0 else times[l]
-            work *= n_add - n
-
-            ratios[eta] = gain / work
-
-        I.append(max(ratios, key=ratios.get))
-
-    return I
+def polynomial_space_V(I, l):
+    V = []
+    for eta in slice_index_set(I, l):
+        k, _ = seperate_index(eta)
+        V.extend(get_power_of_two_index_set(k))
+    return V
 
 class AD_ML_LSQ:
-    def __init__(self):
-        self.I = None
+    def __init__(self, f=None, n_steps=None, d=None):
+        self.f = f
+        self.d = d
+        self.n_steps = n_steps
+        pass
 
-    def __call__(self, x: np.array, grid: bool=False) -> np.array:
+    def __call__(self, x: np.array) -> np.array:
         try:
             projectors = self.projectors_
         except:
             raise ValueError("Model was not fitted yet.")
         else:
-            if grid:
-                return sum(p(x, grid=True) for p in projectors)
-            else:
-                return sum(p(x) for p in projectors)
+            return sum(p(x) for p in projectors)
+
+    def estimate_gain(self, index):
+        neighbours = set(self.I).intersection(set(get_neighbours(index)))
+
+        gain = 0
+        for nbr in neighbours:
+            if nbr in self.gains:
+                gain += self.gains[nbr]
+                continue
+
+            k, l = seperate_index(nbr)
+
+            V = polynomial_space_V(self.I, l)
+            N = int(.1 * get_optimal_sample_size(V, sampling="arcsine"))
+
+            if N > len(self.sample):
+                self.sample = np.vstack(
+                    (self.sample, sample_arcsine((N - len(self.sample), self.d)))
+                )
+
+            evals = self.f_vals.get(l, np.empty(0))
+            times = self.f_times.get(l, np.empty((0, 2)))
+
+            if N > len(evals):
+                s = time.perf_counter()
+                new_evals = self.f(l)(self.sample[len(evals):N])
+                e = time.perf_counter()
+
+                self.f_vals[l] = np.hstack((evals, new_evals))
+                self.f_times[l] = np.vstack((times, np.array([e - s, N - len(evals)])))
+
+            sample = self.sample[:N]
+            f = self.f_vals[l][:N] - self.f_vals[l-1][:N] if l > 0 else self.f_vals[l][:N]
+            space = PolySpace(m=None, d=None)
+            space.index_set = V
+
+            model = LSQ(space, sampling="arcsine").solve(f, sample=sample)
+
+            under_consideration = get_power_of_two_index_set(k)
+            self.gains[nbr] = np.linalg.norm(
+                [v for eta, v in zip(V, model.coef_) if eta in under_consideration]
+            )
+            gain += self.gains[nbr]
+        gain /= len(neighbours)
+
+        return gain
+
+    def estimate_work(self, index):
+        if index in self.works:
+            return self.works[index]
+        k, l = seperate_index(index)
+
+        if l not in self.f_times:
+            s = time.perf_counter()
+            new_evals = self.f(l)(self.sample[0].reshape(1, -1))
+            e = time.perf_counter()
+
+            self.f_vals[l] = new_evals
+            self.f_times[l] = np.expand_dims(np.array([e - s, 1]), axis=0)
+
+        times = sum_times(self.f_times)
+
+        V = polynomial_space_V(self.I, l)
+        n_add = get_optimal_sample_size(V + get_power_of_two_index_set(k), sampling="arcsine")
+        n =  get_optimal_sample_size(V, sampling="arcsine")
+
+        work = times[l] + times[l - 1] if l > 0 else times[l]
+        work *= n_add - n
+
+        self.works[index] = work
+
+        return work
+
+    def construct_multi_index_set(self):
+        """
+        Constructs a problem-dependent, downward closed multi-index set `I` (see Haji-Ali et al.).
+        """
+        self.I = [(0,) * (self.d + 1)]
+        A = [(0,) * i + (1,) + (0,) * (self.d - i) for i in range(self.d + 1)]
+
+        self.sample = np.empty((0, self.d))
+        self.f_vals = {}
+        self.f_times = {}
+
+        self.gains = {}
+        self.works = {}
+        self.ratios = {}
+
+        for _ in range(self.n_steps):
+            ratios = {}
+            for eta in A:
+                gain = self.estimate_gain(eta)
+                work = self.estimate_work(eta)
+
+                ratios[eta] = gain / work
+
+            best_eta = max(ratios, key=ratios.get)
+            self.I.append(best_eta)
+
+            A.remove(best_eta)
+            for nbr in get_neighbours(best_eta, extending=True):
+                if is_downward_closed(self.I + [nbr]):
+                    A.append(nbr)
+
+            self.ratios.update(
+                {index: ratio for index, ratio in ratios.items() if index not in self.ratios}
+            )
+
+        return
 
     def solve(self, f: Callable, n_steps: int, d: int) -> AD_ML_LSQ:
-        if self.I is None:
-            self.I = construct_multi_index_set(f, n_steps, d)
+        self.f = f
+        self.d = d
+        self.n_steps = n_steps
 
-        L = max(eta[-1] for eta in self.I)
+        self.construct_multi_index_set()
+
+        self.L = max(eta[-1] for eta in self.I)
 
         self.projectors_ = []
-        for l in range(L + 1):
-            V = []
-            for eta in slice_index_set(self.I, l):
-                k, _ = seperate_index(eta)
-                V.extend(get_power_of_two_index_set(k))
+        for l in range(self.L + 1):
+            V = polynomial_space_V(self.I, l)
+            N = int(.1 * get_optimal_sample_size(V, sampling="arcsine"))
 
-            N = get_optimal_sample_size(V, sampling="arcsine")
-            sample = sample[:N] if l > 0 else sample_arcsine((N, d))
+            if N > len(self.sample):
+                self.sample = np.vstack(
+                    (self.sample, sample_arcsine((N - len(self.sample), d)))
+                )
 
-            f_l = f(l)
-            f_l__ = f_l_[:N] if l > 0 else np.zeros(N)  # f_{-1} := 0
-            f_l_ = f_l(sample)
-            f_ = lambda _: f_l_ - f_l__
+            evals = self.f_vals.get(l, np.empty(0))
+            if N > evals.size:
+                new_evals = f(l)(self.sample[evals.size:N])
+                self.f_vals[l] = np.hstack((evals, new_evals))
+            if l > 0:
+                evals = self.f_vals.get(l - 1, np.empty(0))
+                if N > evals.size:
+                    new_evals = f(l)(self.sample[evals.size:N])
+                    self.f_vals[l] = np.hstack((evals, new_evals))
 
-            level_l_projector = LSQ(V, sampling="arcsine").solve(f_, sample=sample)
+            sample = self.sample[:N]
+            f_ = self.f_vals[l][:N] - self.f_vals[l - 1][:N] if l > 0 else self.f_vals[l][:N]
+            space = PolySpace(m=None, d=None)
+            space.index_set = V
+
+            level_l_projector = LSQ(space, sampling="arcsine").solve(f_, sample=sample)
             self.projectors_.append(level_l_projector)
 
         return self
 
-    def l2_error(self, sample: list[np.array], values: np.array) -> float:
+    def l2_error(self, sample: np.array, values: np.array) -> float:
         """
         Calculates the L2 error on a grid given by the
         cartesian product of 1-D arrays in `sample`.
         """
-        return np.sqrt(((self(sample, grid=True) - values) ** 2).mean())
+        return np.sqrt(mse(self(sample), values))
