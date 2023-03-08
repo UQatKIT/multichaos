782784b513e9a3f409369ee53f86d22c0e6806d8
Stjepan Salatovic <s.salatovic@yahoo.de>
2023-03-08 22:13:15 +0100

    Inclusion of constants in tuning parameters, general revision

--- src/multilevel_least_squares.py
+++ src/multilevel_least_squares.py
@@ -1,12 +1,12 @@
 """
 Multilevel weighted polynomial least squares.
 """
+from __future__ import annotations
 import numpy as np
 import time
 
-from operator import itemgetter
 from scipy.special import lambertw
-from typing import Callable, Literal
+from typing import Union, Optional
 
 from sampling import sample_optimal_distribution
 from polynomial_spaces import PolySpace
@@ -14,166 +14,155 @@ from least_squares import SingleLevelLSQ
 from utils import mse
 
 
-PolynomialSpace = Literal[
-    "TP",   # tensor product
-    "TD",   # total degree
-    "HC",   # hyperbolic cross
-]
-
-
-def set_asymptotics(params: dict) -> tuple[float, float]:
-    """
-    Calculates two asymptotic parameters for the work of the multilevel algorithm.
-    """
-    beta, gamma, sigma, alpha = itemgetter("beta", "gamma", "sigma", "alpha")(params)
-
-    c = gamma / beta - sigma / alpha
-    l = max(gamma / beta, sigma / alpha)
-
-    if c > 0:
-        t = 1
-    elif c < 0:
-        t = 2
-    else:
-        t = 3 + sigma / alpha
-
-    return l, t
-
-def get_number_of_levels(eps: float, params: dict) -> int:
-    """
-    Returns the number of levels `L` required to achieve the
-    theoretical residual and work bounds given by the tolerance `eps`.
-    """
-    beta, gamma, sigma, alpha = itemgetter("beta", "gamma", "sigma", "alpha")(params)
-
-    c = gamma / beta - sigma / alpha
-    if c > 0:
-        L = - (gamma + beta) / beta * np.log(eps)
-    elif c < 0:
-        L = - (sigma + alpha) / alpha * np.log(eps)
-    else:
-        grid = np.linspace(1, 100, 1000)
-        ix = np.argmin(np.abs(np.exp(-grid * alpha / (sigma + alpha)) * (grid + 1) - eps))
-        L = grid[ix]
-
-    L = int(np.ceil(L))
-
-    return L
-
-def get_tuning_params(L: int, params: dict) -> tuple[np.array, np.array, np.array]:
-    """
-    Returns the tuning parameters for the multilevel algorithm,
-    given problem-dependent constants for specific assumptions.
-    """
-    beta, gamma, sigma, alpha = itemgetter("beta", "gamma", "sigma", "alpha")(params)
-
-    mk = np.exp(np.arange(L + 1) / (sigma + alpha))
-    nl = np.exp(np.arange(L + 1) / (gamma + beta))
-
-    r = 1
-    kappa = (1 - np.log(2)) / (1 + r) / 2
-    aux = mk ** sigma
-    Ns = np.ceil(np.real(np.exp(-lambertw(-kappa / aux, k=-1))))
-
-    mk = np.ceil(mk).astype(int)
-    nl = np.ceil(nl).astype(int)
-
-    return mk, nl, Ns
-
-def print_start(eps: float, L: int, mk: np.array, nl: np.array, Ns: np.array, reduce: float, reuse_sample: bool) -> list[int]:
-    w = [max(map(len, map(str, arr))) for arr in [mk, nl, Ns]]
-    verboseprint(f"eps = {eps:.2e}, L = {L}, reduce = {100 * reduce}%, reuse_sample = {reuse_sample}")
-    verboseprint(f"{'Level':>5} | {'m':>{w[0]}} | {'n':>{w[1]}} | {'N':>{w[2]}} | {'time':>7}")
-    verboseprint("-" * (24 + sum(w)))
-    return w
-
-def print_level(l: int, m: int, n: int, N: int, time: float, w: list[int]):
-    verboseprint(f"{l:>5} | {m:>{w[0]}} | {n:>{w[1]}} | {N:>{w[2]}} | {time:>6.3f}s")
-
-def print_end(time):
-    verboseprint(f"TOTAL TIME: {time:.3f}s\n")
-
-
 class MultiLevelLSQ:
-    def __init__(self, params: dict, dim: int, poly_space: PolynomialSpace) -> None:
-        self.params_ = params
-        self.dim_ = dim
-        self.poly_space = poly_space
+    def __init__(self, problem: dict, params: dict):
+        self.problem = problem
+        self.params = params
 
-        self.asymptotics_ = set_asymptotics(self.params_)
+        self.beta = problem["beta"]
+        self.gamma = problem["gamma"]
+        self.sigma = problem["sigma"]
+        self.alpha = problem["alpha"]
+
+        self.response = problem["response"]
+        self.dim = problem["dim"]
+
+        self.reuse_sample = params["reuse_sample"]
+        self.reduce_sample_by = params["reduce_sample_by"]
+        self.poly_space = params["poly_space"]
+        self.verbose = params["verbose"]
+
+        self.C_m = params["C_m"]
+        self.C_n = params["C_n"]
+
+        self.asymptotics()
+
+    def asymptotics(self):
+        b, g, s, a = self.beta, self.gamma, self.sigma, self.alpha
+        case = g / b - s / a
+        self.lambda_ = max(g / b, s / a)
+        if case > 0:
+            self.t = 1
+        elif case < 0:
+            self.t = 2
+        else:
+            self.t = 3 + s / a
+
+    def number_of_levels(self, eps: float) -> int:
+        b, g, s, a = self.beta, self.gamma, self.sigma, self.alpha
+        case = g / b - s / a
+        if case > 0:
+            L = - (g + b) / b * np.log(eps)
+        elif case < 0:
+            L = - (s + a) / a * np.log(eps)
+        else:
+            grid = np.linspace(1, 100, 1000)
+            ix = np.argmin(np.abs(np.exp(-grid * a / (s + a)) * (grid + 1) - eps))
+            L = grid[ix]
+        self.L = np.ceil(L).astype(int)
+        return self.L
+
+    def tuning_params(self, L: int):
+        b, g, s, a = self.beta, self.gamma, self.sigma, self.alpha
+
+        self.mk = self.C_m * np.exp(np.arange(L + 1) / (s + a))
+        self.nl = self.C_n * np.exp(np.arange(L + 1) / (g + b))
+
+        self.Ns = self.sample_sizes(self.mk)
+
+        self.mk = np.ceil(self.mk).astype(int)
+        self.nl = np.ceil(self.nl).astype(int)
+
+        return self.mk, self.nl
+
+    def sample_sizes(self, mk):
+        r = 1
+        kappa = (1 - np.log(2)) / (1 + r) / 2
+        aux = mk ** self.sigma
+        Ns = np.ceil(np.real(np.exp(-lambertw(-kappa / aux, k=-1))))
+        Ns = ((1 - self.reduce_sample_by) * Ns).astype(int)
+        return Ns
+
+    def work(self) -> float:
+        gamma = self.gamma
+        Ns = self.Ns
+        nl = self.nl
+        if self.reuse_sample:
+            work = (Ns[::-1] * nl ** gamma).sum()
+        else:
+            work = Ns[-1] * nl[0] ** gamma + (Ns[:-1][::-1] * (nl[1:] ** gamma + nl[:-1] ** gamma)).sum()
+        return work
+
+    def log(self, level: Union[str, int], runtime: Optional[float]=None):
+        if level == "start":
+            self.v_print = print if self.verbose else lambda *a, **k: None
+            w = [max(map(len, map(str, arr))) for arr in [self.mk, self.nl, self.Ns]]
+            self.v_print(
+                f"eps = {self.eps:.2e}, L = {self.L}, reduce = {100 * self.reduce_sample_by}%, reuse_sample = {self.reuse_sample}"
+            )
+            self.v_print(f"{'Level':>5} | {'m':>{w[0]}} | {'n':>{w[1]}} | {'N':>{w[2]}} | {'time':>7}")
+            self.v_print("-" * (24 + sum(w)))
+            self.print_widths = w
+        elif level == "end":
+            self.v_print(f"Total runtime: {runtime:.3f}s\n")
+        else:
+            w = self.print_widths
+            m = self.mk[self.L - level]
+            n = self.nl[level]
+            N = self.Ns[self.L - level]
+            self.v_print(f"{level:>5} | {m:>{w[0]}} | {n:>{w[1]}} | {N:>{w[2]}} | {runtime:>6.3f}s")
+
+    def fit(self, eps: float) -> MultiLevelLSQ:
+        start = time.perf_counter()
+
+        self.eps = eps
+
+        L = self.number_of_levels(eps)
+        mk, nl = self.tuning_params(L)
+        Ns = self.Ns
+
+        self.log(level="start")
+
+        if self.reuse_sample:
+            I = PolySpace(self.poly_space, max(mk), self.dim).index_set
+            sample = sample_optimal_distribution(I, max(Ns))
+
+        self.projectors = []
+        for l in range(L + 1):
+            start_l = time.perf_counter()
+
+            m = mk[L - l]
+            n = nl[l]
+            N = Ns[L - l]
+
+            V = PolySpace(self.poly_space, m, self.dim)
+            projector = SingleLevelLSQ(V, sampling="optimal")
+            if not self.reuse_sample:
+                f_l = self.response(n)
+                f_l_ = self.response(nl[l - 1]) if l > 0 else lambda _: 0
+                f_ = lambda g: f_l(g) - f_l_(g)
+                projector.fit(f_, N=N)
+            else:
+                sample_l = sample[:N]
+                f_l = self.response(n)
+                f_l__ = f_l_[:N] if l > 0 else np.zeros(N)
+                f_l_ = f_l(sample_l)
+                projector.fit(f_l_ - f_l__, sample=sample_l)
+
+            self.projectors.append(projector)
+
+            self.log(l, runtime=time.perf_counter() - start_l)
+        self.log("end", runtime=time.perf_counter() - start)
+
+        return self
 
     def __call__(self, x: np.array) -> np.array:
         try:
-            projectors = self.projectors_
+            projectors = self.projectors
         except:
             raise ValueError("Model was not fitted yet.")
         else:
             return sum(p(x) for p in projectors)
 
-    def fit(self, f: Callable, eps: float, opt: dict) -> np.array:
-        """
-        Returns the coefficients `v` w.r.t the total degree basis on each level `l = 0, ..., L`.
-        """
-        reuse_sample, reduce_sample_by, verbose = itemgetter(
-            "reuse_sample", "reduce_sample_by", "verbose"
-        )(opt)
-
-        global verboseprint
-        verboseprint = print if verbose else lambda *a, **k: None
-
-        s_total = time.perf_counter()
-
-        L = get_number_of_levels(eps, self.params_)
-        mk, nl, Ns = get_tuning_params(L, self.params_)
-        Ns = np.ceil((1 - reduce_sample_by) * Ns).astype(int)
-
-        self.L = L
-        self.mk = mk
-        self.nl = nl
-        self.Ns = Ns
-
-        w = print_start(eps, L, mk, nl, Ns, reduce_sample_by, reuse_sample)
-
-        if reuse_sample:
-            I = PolySpace(self.poly_space, max(mk), d=self.dim_).index_set
-            sample = sample_optimal_distribution(I, max(Ns))
-
-        self.projectors_ = []
-        for l in range(L + 1):
-            s = time.perf_counter()
-
-            m = mk[L - l]
-            N = Ns[L - l]
-            n = nl[l]
-
-            V_m = PolySpace(self.poly_space, m, d=self.dim_)
-            level_l_projector = SingleLevelLSQ(V_m, sampling="optimal")
-            if not reuse_sample:
-                f_l = f(n)
-                f_l_ = f(nl[l - 1]) if l > 0 else lambda _: 0  # f_{-1} := 0
-                f_ = lambda g: f_l(g) - f_l_(g)
-                level_l_projector.fit(f_, N=N)
-            else:
-                sample_l = sample[:N]
-                f_l = f(n)
-                f_l__ = f_l_[:N] if l > 0 else np.zeros(N)  # f_{-1} := 0
-                f_l_ = f_l(sample_l)
-                level_l_projector.fit(f_l_ - f_l__, sample=sample_l)
-
-            self.projectors_.append(level_l_projector)
-
-            e = time.perf_counter()
-
-            print_level(l, m, n, N, e - s, w)
-
-        e_total = time.perf_counter()
-        print_end(e_total - s_total)
-
-        return self
-
-    def l2_error(self, sample: np.array, values: np.array) -> float:
-        """
-        Calculates the L2 error on a grid given by the
-        cartesian product of 1-D arrays in `sample`.
-        """
-        return np.sqrt(mse(self(sample), values))
+    def l2_error(self, x: np.array, y: np.array) -> float:
+        return np.sqrt(mse(self(x), y))
