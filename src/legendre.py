ffaf7123972b99d2b6a45a801d9258013dcaba9f
Stjepan Salatovic <s.salatovic@yahoo.de>
2023-03-24 20:42:49 +0100

    Move evaluate basis to Legendre module

--- src/legendre.py
+++ src/legendre.py
@@ -3,6 +3,11 @@ Methods for constructing basis and evaluation of Legendre polynomials.
 """
 import numpy as np
 
+from typing import Union
+
+
+IndexSet = list[Union[int, tuple[int, ...]]]
+
 
 def legval(n: int, sample: np.array) -> np.array:
     """
@@ -18,3 +23,29 @@ def legval(n: int, sample: np.array) -> np.array:
             y[i + 1] = ((2 * i + 1) * sh_sample * y[i] - i * y[i - 1]) / (i + 1)
 
     return y
+
+def evaluate_basis(I: IndexSet, sample: np.array) -> np.array:
+    I = np.array(I)
+
+    dim = sample.ndim
+    if dim == 1:
+        basis_val = legval(np.max(I), sample)[I]
+    else:
+        max_idxs = np.max(I, axis=0)
+        basis_val_uni = {}
+        for j in range(dim):
+            basis_val_uni[j] = legval(max_idxs[j], sample[:, j])
+
+        basis_val = np.zeros((len(I), len(sample)))
+        for i, eta in enumerate(I):
+            prod = 1.
+            for j, k in enumerate(eta):
+                prod *= basis_val_uni[j][k]
+            basis_val[i] = prod
+
+    norms = np.sqrt((2 * I + 1))
+    if dim > 1:
+        norms = norms.prod(axis=1)
+    basis_val *= norms.reshape(-1, 1)
+
+    return basis_val
